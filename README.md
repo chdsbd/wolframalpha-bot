739a3c1add98681d9e072ad2602d144eba1fc683
Christopher Dignam <me@christopher.xyz>
2016-09-02 19:59:27 -0400

    Added setup instructions to README and gunicorn

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # wolframalpha-bot
+> A simple Flask app that provides a webhook for Slack to query the WolframAlpha API
+
+## Setup
+
+1. Configure the Flask App using your [WolframAlphaAPIKey](http://products.wolframalpha.com/api/):
+```bash
+export API_KEY=<WolframAlphaAPIKey>
+export TOKEN=<MakeALongTokenToSendWithYourRequests>
+```
+
+2. Configure an [Outgoing WebHook](https://slack.com/apps/A0F7VRG6Q-outgoing-webhooks) for Slack with your `URL` and `TOKEN`.
+
+3. Start app
+  - production: run `gunicorn app:app`
+  - development: run `python app.py`
--- app.py
+++ app.py
@@ -8,18 +8,16 @@ TOKEN = os.getenv('TOKEN')
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def main():
     if API_KEY is None or TOKEN is None:
         return 'Missing configuration for API_KEY and/or TOKEN', 500
-    if request.method == 'GET':
-        return 'Looking good!'
     if request.form['token'] != TOKEN:
         return 'Unauthorized', 401
     query = request.form['text']
     client = wolframalpha.Client(API_KEY)
     res = client.query(query)
     return jsonify({'text': next(res.results).text})
-    
+
 if __name__ == "__main__":
     app.run(debug=True)

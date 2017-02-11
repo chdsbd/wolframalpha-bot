0854743ea81c5311aee8b4454447e7a6151f08b8
Christopher Dignam <me@christopher.xyz>
2017-02-11 10:37:11 -0500

    Added GET method so service can be watched

--- app.py
+++ app.py
@@ -10,24 +10,27 @@ TOKEN = os.getenv('TOKEN')
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def main():
-    if API_KEY is None or TOKEN is None:
-        return 'Missing configuration for API_KEY and/or TOKEN', 500
-    if request.form['token'] != TOKEN:
-        return 'Unauthorized', 401
-    # https://docs.python.org/3/library/stdtypes.html#str.split
-    query = request.form['text'].split(' ', 1)[1]
-    client = wolframalpha.Client(API_KEY)
-    res = client.query(query)
+    if request.method == 'POST':
+        if API_KEY is None or TOKEN is None:
+            return 'Missing configuration for API_KEY and/or TOKEN', 500
+        if request.form['token'] != TOKEN:
+            return 'Unauthorized', 401
+        # https://docs.python.org/3/library/stdtypes.html#str.split
+        query = request.form['text'].split(' ', 1)[1]
+        client = wolframalpha.Client(API_KEY)
+        res = client.query(query)
 
-    if len(res.pods) == 0:
-        return jsonify({'text': "Sorry, I couldn't find any relevant information for you."})
-    try:
-        response = next(res.results).text
-    except StopIteration:
-        response = "Sorry, I couldn't get a result for that query."
-    return jsonify({'text': response})
+        if len(res.pods) == 0:
+            return jsonify({'text': "Sorry, I couldn't find any relevant information for you."})
+        try:
+            response = next(res.results).text
+        except StopIteration:
+            response = "Sorry, I couldn't get a result for that query."
+        return jsonify({'text': response})
+    if request.method == 'GET':
+        return 'See https://github.com/chdsbd/wolframalpha-bot'
 
 
 if __name__ == "__main__":
